996d994436e9d941540eca4bbe20c751ac4acba0
Dung Nguyen <cdung2508@gmail.com>
2026-08-07 17:35:27 +0900

    add dashboard page and refactor code

--- sw.js
+++ sw.js
@@ -3,6 +3,10 @@ const CACHE = "offline-v1";
 const FILES = [
   "/flashcard-N5/",
   "/flashcard-N5/index.html",
+  "/flashcard-N5/lessons.html",
+  "/flashcard-N5/base.css",
+  "/flashcard-N5/index.css",
+  "/flashcard-N5/lessons.css",
   "/flashcard-N5/index.js",
   "/flashcard-N5/lessons.js"
 ];
